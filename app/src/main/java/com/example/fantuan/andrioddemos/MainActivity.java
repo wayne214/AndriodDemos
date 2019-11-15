d18c0302c7d6279a28330bf7568bc3a12590beec
wayne <jun_wei_like1@163.com>
2019-11-15 16:46:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fantuan/andrioddemos/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.fantuan.andrioddemos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.fantuan.andrioddemos.adapters.MyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemClickListener {
+
+    private RecyclerView myRecylerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private MyAdapter adapter;
+    private List<String> datas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDatas();
+        initView();
+    }
+
+    private void initView() {
+        myRecylerView = findViewById(R.id.my_recycler_view);
+        myRecylerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        myRecylerView.setLayoutManager(layoutManager);
+
+
+        adapter = new MyAdapter(datas);
+
+        myRecylerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(this);
+
+    }
+
+    private void initDatas() {
+        datas = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            datas.add("数据"+i);
+        }
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(MainActivity.this,"Click:"+position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemLongClick(View view, int position) {
+        adapter.delateData(position);
+    }
+}
---
+++ app/src/main/java/com/example/fantuan/andrioddemos/adapters/MyAdapter.java
@@ -0,0 +1,97 @@
+package com.example.fantuan.andrioddemos.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.fantuan.andrioddemos.R;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    // 添加点击事件
+
+    public interface OnItemClickListener{
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view, int position);
+    }
+
+    public OnItemClickListener listener;
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+
+
+    private List<String> mDataSet;
+
+    public MyAdapter(List<String> myDateSet) {
+        mDataSet = myDateSet;
+    }
+    // 删除一条数据，有动画效果
+    public void delateData(int position){
+        mDataSet.remove(position);
+        notifyItemRemoved(position);
+    }
+    // 添加一条数据，有动画效果
+    public void addData(int position) {
+        if (position >= 1) {
+            mDataSet.add(mDataSet.get(position));
+            notifyItemInserted(position);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+       View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.my_text_view, viewGroup, false);
+        MyViewHolder viewHolder = new MyViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        myViewHolder.textView.setText(mDataSet.get(i));
+
+        setUpItemEvent(myViewHolder);
+    }
+
+    private void setUpItemEvent(MyViewHolder holder) {
+        if (listener != null) {
+            final int layoutPosition = holder.getLayoutPosition();
+            holder.textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(v,layoutPosition);
+                }
+            });
+
+            holder.textView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    listener.onItemLongClick(v, layoutPosition);
+                    return false;
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.item_tv);
+        }
+    }
+}
