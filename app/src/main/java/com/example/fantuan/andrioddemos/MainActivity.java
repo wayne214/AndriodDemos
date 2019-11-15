322f83a03c1ef52de79d0f92b1d910e7cfec0f2d
wayne <jun_wei_like1@163.com>
2019-11-15 17:33:38 +0800

    集成下拉刷新，上拉加载库--SmartRefreshLayout

--- app/src/main/java/com/example/fantuan/andrioddemos/MainActivity.java
+++ app/src/main/java/com/example/fantuan/andrioddemos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fantuan.andrioddemos;
 
+import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,18 +10,23 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.fantuan.andrioddemos.adapters.MyAdapter;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.header.BezierRadarHeader;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener {
+public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemClickListener, OnRefreshListener, OnLoadMoreListener {
 
     private RecyclerView myRecylerView;
     private RecyclerView.LayoutManager layoutManager;
     private MyAdapter adapter;
     private List<String> datas = new ArrayList<>();;
 
-    private SwipeRefreshLayout swipeRefreshLayout;
+    private SmartRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +39,11 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemC
     private void initView() {
         swipeRefreshLayout = findViewById(R.id.swiperefresh);
 
+        swipeRefreshLayout.setRefreshHeader(new BezierRadarHeader(this).setEnableHorizontalDrag(true));
+
         swipeRefreshLayout.setOnRefreshListener(this);
+
+        swipeRefreshLayout.setOnLoadMoreListener(this);
 
         myRecylerView = findViewById(R.id.my_recycler_view);
         myRecylerView.setHasFixedSize(true);
@@ -65,13 +75,21 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemC
     public void onItemLongClick(View view, int position) {
         adapter.delateData(position);
     }
-    // 下拉刷新
 
     @Override
-    public void onRefresh() {
+    public void onRefresh(@NonNull RefreshLayout refreshLayout) {
         datas.clear();
         initDatas();
-        swipeRefreshLayout.setRefreshing(false);
+        swipeRefreshLayout.finishRefresh();
+
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+        datas.addAll(datas);
+        swipeRefreshLayout.finishLoadMore();
+
         adapter.notifyDataSetChanged();
     }
 }
