799bc1e6286a2507996d71bedb15f5cc94f55273
wayne <jun_wei_like1@163.com>
2020-07-23 14:58:44 +0800

    lrucache

---
+++ app/src/main/java/com/example/fantuan/andrioddemos/lrucache/setLrucache.java
@@ -0,0 +1,29 @@
+package com.example.fantuan.andrioddemos.lrucache;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+// 设置图片缓存大小
+public class setLrucache {
+    private LruCache<String, Bitmap> memoryCache = new LruCache<String, Bitmap> (cacheSize) {
+        @Override
+        protected int sizeOf(String key, Bitmap bitmap) {
+//            return super.sizeOf(key, value);
+            return bitmap.getByteCount() / 1024;
+        }
+    };
+
+
+    final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+    final int cacheSize = maxMemory / 8;
+
+    public void addBitmapToMemortyCache(String key, Bitmap bitmap) {
+        if(getBitmapFromMemCache(key) ==null) {
+            memoryCache.put(key, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromMemCache(String key) {
+        return memoryCache.get(key);
+    }
+}
