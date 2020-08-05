8a0673740cb14fd0d5f5e5451131ccc72af9a8c6
wayne <jun_wei_like1@163.com>
2020-08-05 14:20:40 +0800

    android注解框架ButterKnife

--- app/src/main/java/com/example/fantuan/andrioddemos/MainActivity.java
+++ app/src/main/java/com/example/fantuan/andrioddemos/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.fantuan.andrioddemos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.fantuan.andrioddemos.customView.PieImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.pieImageView) PieImageView pieImageView;
+    @OnClick(R.id.pieImageView) void click() {
+        Toast.makeText(this, "单击了吗？", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,5 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         pieImageView.setProgress(50);
     }
-
 
 }
