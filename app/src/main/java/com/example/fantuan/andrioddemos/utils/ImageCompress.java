12dc81eebbc43bfd0cd459b05b78b1ae69eefa88
wayne <jun_wei_like1@163.com>
2020-08-13 16:18:04 +0800

    图片压缩--设置图片格式

--- app/src/main/java/com/example/fantuan/andrioddemos/utils/ImageCompress.java
+++ app/src/main/java/com/example/fantuan/andrioddemos/utils/ImageCompress.java
@@ -15,6 +15,18 @@ import java.io.FileOutputStream;
  * 图片压缩
  * */
 public class ImageCompress {
+
+    /**
+     * 设置图片格式
+     * Android目前常用的图片格式有png，jpeg和webp，
+     png：无损压缩图片格式，支持Alpha通道，Android切图素材多采用此格式
+     jpeg：有损压缩图片格式，不支持背景透明，适用于照片等色彩丰富的大图压缩，不适合logo
+     webp：是一种同时提供了有损压缩和无损压缩的图片格式，派生自视频编码格式VP8，从谷歌官网来看，无损webp平均比png小26%，有损的webp平均比jpeg小25%~34%，
+     无损webp支持Alpha通道，有损webp在一定的条件下同样支持，有损webp在Android4.0（API 14）之后支持，无损和透明在Android4.3（API18）之后支持
+     采用webp能够在保持图片清晰度的情况下，可以有效减小图片所占有的磁盘空间大小
+     *
+     * */
+
     /**
      * 1.质量压缩
      * 设置bitmap options属性，降低图片质量，像素不减
